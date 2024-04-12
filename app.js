ca6689a6b7de37488ae6701f1c93bfffd8b747b1
TirumalaArikatla <156239185+TirumalaArikatla@users.noreply.github.com>
2024-04-12 09:15:35 -0500

    lab

--- app.js
+++ app.js
@@ -10,10 +10,28 @@ var gRouter = require('./routes/grid');
 var rRouter = require('./routes/pick');
 var pRouter = require('./routes/pen');
 var pen = require("./models/pen");
-
 var resourceRouter = require('./routes/resource');
 var app = express();
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,6 +40,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -30,6 +56,13 @@ app.use('/grid', gRouter);
 app.use('/ran', rRouter);
 app.use('/pen', pRouter);
 app.use('/resource', resourceRouter);
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
