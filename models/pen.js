1c0b32a9823684a01a5b4b4a9308c329cbda0741
TirumalaArikatla <156239185+TirumalaArikatla@users.noreply.github.com>
2024-04-17 01:08:09 -0500

    12

--- models/pen.js
+++ models/pen.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 const penSchema = mongoose.Schema({
 pen_name: String,
-ink: String,
-cost: Number
+ink: {
+    type: String,
+    minlength: 1,
+    maxlength: 19,
+
+},
+cost: {
+    type:Number,
+    min : 1,
+    max : 234,
+}
 })
 module.exports = mongoose.model("pen",
 penSchema)
--- routes/pen.js
+++ routes/pen.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var passport = require('passport');
 const pen_controlers= require('../controllers/pen');
 var router = express.Router();
 /* GET costumes */
 router.get('/', pen_controlers.pen_view_all_Page );
 /* GET detail costume page */
 router.get('/detail', pen_controlers.pen_view_one_Page);
-router.get('/create', pen_controlers.pen_create_Page);
+//router.get('/create', pen_controlers.pen_create_Page);
 /* GET create update page */
-router.get('/update', pen_controlers.pen_update_Page);
-router.get('/delete', pen_controlers.pen_delete_Page);
+//router.get('/update', pen_controlers.pen_update_Page);
+//router.get('/delete', pen_controlers.pen_delete_Page);
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+router.get('/create',secured, pen_controlers.pen_create_Page);
+router.get('/update',secured, pen_controlers.pen_update_Page);
+router.get('/delete',secured, pen_controlers.pen_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
 module.exports = router;
